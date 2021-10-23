74534a87a299bdd4fce35a529585b4a7df5eb5ab
Thorsten Lorenz <thlorenz@gmx.de>
2021-10-22 20:20:26 -0500

    chore: init js project

---
+++ transfer-lamports/js/init-deps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+yarn add -D               \
+    @tsconfig/recommended \
+    @types/node           \
+    debug                 \
+    prettier              \
+    supports-color        \
+    ts-node               \
+    tslib                 \
+    typescript 
+
+yarn add            \
+    borsh           \
+    bs58            \
+    @solana/web3.js \
+    @types/debug    \
